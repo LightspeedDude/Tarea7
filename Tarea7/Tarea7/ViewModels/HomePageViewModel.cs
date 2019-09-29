39e3a07a8a1fa2f395014bdeab221046b8de408d
LightspeedDude <julio_martinez25@live.com>
2019-09-29 15:33:36 -0400

    changes

--- Tarea7/Tarea7/ViewModels/HomePageViewModel.cs
+++ Tarea7/Tarea7/ViewModels/HomePageViewModel.cs
@@ -8,7 +8,7 @@ namespace Tarea7.ViewModels
     {
         public string Givename { get; set; }
         public string Name { get; set; } = Givename;
-        public bool Lucky { get; set; }
+        public bool Lucky { get; set; } = true;
     }
 
 
