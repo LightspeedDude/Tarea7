85f95a731eb60476b1ea345179c4366ba0daf5fa
LightspeedDude <julio_martinez25@live.com>
2019-09-29 14:00:36 -0400

    tarea

--- Tarea7/Tarea7/ViewModels/HomePageViewModel.cs
+++ Tarea7/Tarea7/ViewModels/HomePageViewModel.cs
@@ -7,7 +7,7 @@ namespace Tarea7.ViewModels
     public class HomePageViewModel
     {
         public string Name { get; set; } = "Username";
-        public DateTime Day { get; set; }
+        public DateTime Day { get; set; }  
 
         public bool IsAdmin { get; set; }
     }
